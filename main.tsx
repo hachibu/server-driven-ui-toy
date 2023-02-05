3d34c0a8e8027ab9925cc125a6d2c2d619cd4484
Raymond Sohn <raymondsohn@gmail.com>
2023-02-04 20:29:48 -0800

    Create map of client components
    Create map of client components to simplify component lookup.

--- main.tsx
+++ main.tsx
@@ -1,35 +1,33 @@
 import * as React from "npm:react";
-import * as ReactDOMServer from "npm:react-dom/server";
+import { renderToString } from "npm:react-dom/server";
 
 interface ServerComponent {
-  type: string
-  props: Record<string, string>
+  type: string;
+  props: Record<string, string>;
 }
 
 function getServerComponent(): ServerComponent {
   return {
-    type: 'HelloComponent',
+    type: "HelloComponent",
     props: {
-      subject: "Server-Driven UI"
-    }
-  }
+      subject: "Server-Driven UI",
+    },
+  };
 }
 
-function HelloComponent({ subject }: ServerComponent['props']) {
-  return <div>Hello {subject}</div>;
-}
+const CLIENT_COMPONENTS: Record<string, ReturnType<React.Component>> = {
+  HelloComponent({ subject }: ServerComponent["props"]) {
+    return <div>Hello {subject}</div>;
+  },
+};
 
 function main() {
-  const serverComponent = getServerComponent()
+  const serverComponent = getServerComponent();
+  const ClientComponent = CLIENT_COMPONENTS[serverComponent.type];
 
-  let clientComponent;
-  switch (serverComponent.type) {
-    case 'HelloComponent':
-      clientComponent = <HelloComponent {...serverComponent.props}></HelloComponent>
-      break
-  }
-
-  console.log(ReactDOMServer.renderToString(clientComponent))
+  return renderToString(
+    <ClientComponent {...serverComponent.props}></ClientComponent>
+  );
 }
 
-if (import.meta.main) main();
+if (import.meta.main) console.log(main())
